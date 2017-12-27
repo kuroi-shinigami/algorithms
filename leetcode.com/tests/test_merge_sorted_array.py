dd8bdb04197b4124b157acefadb14013263726bd
kuroi-shinigami <Ri.Shenshun.bk.201@gmail.com>
2017-12-27 21:58:49 +0300

    88. Merge Sorted Array

---
+++ leetcode.com/tests/test_merge_sorted_array.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+
+from merge_sorted_array import Solution
+
+s = Solution()
+
+
+class TestSolution(TestCase):
+    """"""
+    def test_0(self):
+        self.assertEqual(s.merge([0], 0, [1], 1), [0, 1])
+
+    def test_1(self):
+        self.assertEqual(s.merge([1], 1, [], 0), [1])
+
+    # def test_(self):
+    #     self.assertEqual(s.merge([], , [], ), [])
