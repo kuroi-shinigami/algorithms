e7bfbf9987b73e9c8e0391dd2ac0b3cac99b9c11
kuroi-shinigami <Ri.Shenshun.bk.201@gmail.com>
2018-01-20 11:44:23 +0300

    521. Longest Uncommon Subsequence I

---
+++ leetcode.com/longest_uncommon_subsequence_I.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+
+class Solution:
+    def findLUSlength(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: int
+        """
+        return max(len(a), len(b)) if a != b else -1
---
+++ leetcode.com/tests/test_longest_uncommon_subsequence_I.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+
+
+from longest_uncommon_subsequence_I import Solution
+
+s = Solution()
+
+
+class TestSolution(TestCase):
+    """"""
+
+    def test_0(self):
+        self.assertEqual(s.findLUSlength('aba', 'cdc'), 3)
+
+    def test_1(self):
+        self.assertEqual(s.findLUSlength("aaa", "aaa"), -1)
+
+    def test_2(self):
+        self.assertEqual(s.findLUSlength("aefawfawfawfaw", "aefawfeawfwafwaef"), 17)
+
+    def test_3(self):
+        self.assertEqual(s.findLUSlength("", ""), -1)
+
+    # def test_(self):
+    #     self.assertEqual(s.findLUSlength(), )
