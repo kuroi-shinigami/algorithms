af5ab7bdba0088c0b0f62efaa619aa879aa10535
kuroi-shinigami <Ri.Shenshun.bk.201@gmail.com>
2018-03-08 22:05:05 +0300

    415. Add Strings

---
+++ leetcode.com/add_strings.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+
+class Solution:
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        return str(int(num1)+int(num2))
