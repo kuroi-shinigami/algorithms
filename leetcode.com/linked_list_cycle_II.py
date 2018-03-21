c7c9c2031ea23f86c3f051c56db2b0af04cb20e3
kuroi-shinigami <Ri.Shenshun.bk.201@gmail.com>
2018-03-21 10:24:18 +0300

    278. First Bad Version

---
+++ leetcode.com/linked_list_cycle_II.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        _seen = set()
+        h = head
+        while h:
+            if h not in _seen:
+                _seen.add(h)
+                h = h.next
+            else:
+                return h
+        return None
